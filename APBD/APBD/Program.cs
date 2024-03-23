58bde6ab79f94290a1aea6ae5d62ebed919c4b0d
s26107 <s26107@pjwstk.edu.pl>
2024-03-23 18:24:35 +0100

    function for avg added

--- APBD/APBD/Program.cs
+++ APBD/APBD/Program.cs
@@ -1,5 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+static float checkAvg(int[] ints)
+{
+    int sum = 0;
+    foreach (var val in ints)
+    {
+        sum += val;
+    }
+
+    return sum / ints.Length;
+}
+
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Modyfikacja 1");
@@ -7,3 +18,7 @@ Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 
 Console.WriteLine("Modyfikacja 3");
+
+int[] ints = new[] { 1, 2, 3, 4, 5 };
+
+Console.WriteLine(checkAvg(ints));
